ea2b906b5c453a45d71e0f8d290948ad89b5fea5
carlos-castiblanco <carlos.castiblanco@globant.com>
2022-09-17 14:30:40 -0500

    :construction: CC Add Postman Collections and fixes functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './product/product.module';
 import { StoreModule } from './store/store.module';
 import { StoreProductModule } from './store-product/store-product.module';
+import { ProductEntity } from './product/product.entity';
+import { StoreEntity } from './store/store.entity';
 
 @Module({
-  imports: [ProductModule, StoreModule, StoreProductModule],
+  imports: [
+    ProductModule,
+    StoreModule,
+    StoreProductModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'gastronomic_culture',
+      entities: [ProductEntity, StoreEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/store-product/store-product.module.ts
+++ src/store-product/store-product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StoreProductService } from './store-product.service';
 import { StoreProductController } from './store-product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoreEntity } from '../store/store.entity';
+import { ProductEntity } from '../product/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StoreEntity, ProductEntity])],
   providers: [StoreProductService],
-  controllers: [StoreProductController]
+  controllers: [StoreProductController],
 })
 export class StoreProductModule {}
